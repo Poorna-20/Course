cd2329a153c70f05104c249b3a5df4b63491d36b
CH POORNA CHANDRA <21pa1a0533@vishnu.edu.in>
2024-02-22 23:13:10 +0530

    add-course test is added

---
+++ __tests__/add-course.test.js
@@ -0,0 +1,67 @@
+const request = require('supertest');
+const app = require('../app');
+const db = require('../models/index');
+const PORT = process.env.PORT || 3000;
+
+let server,agent;
+
+describe('Add Course Route', () => {
+    beforeAll(async() => {
+      await db.sequelize.sync({force:true})
+      // Start the server
+      server = app.listen(PORT, () => {  });
+      agent = request.agent(server);
+    });
+    afterAll(async() => {
+      // Close the server
+      await db.sequelize.close();
+      server.close();
+    });
+    test('should add a new course and redirect to admin dashboard', async () => {
+
+      // Simulate authentication as an admin
+    const agent = request.agent(app);
+    await agent.post('/login').send({ email: 'admin@example.com', password: 'adminpassword' });
+
+
+      const courseData = {
+        title: 'New Course',
+        description: 'Description of the new course',
+        contentText: 'Content text',
+        contentUrl: 'https://example.com/content'
+      };
+
+      const res = await agent
+        .post('/add-course')
+        .send(courseData);
+      expect(res.status).toBe(302);
+      expect(res.header.location).toBe('/admin-dashboard');
+    });
+  });
+  
+// const request = require('supertest');
+// const app = require('../app');
+// const bcrypt = require('bcrypt');
+
+// describe('Add Course Route', () => {
+//     test('should add a new course and redirect to admin dashboard', async () => {
+//       // Simulate authentication as an admin
+//       const agent = request.agent(app);
+//       const hashedPassword = await bcrypt.hash('adminpassword', 10); 
+//       console.log("pass : ",hashedPassword);// Hash the password
+//       await agent.post('/login').send({ email: 'admin@example.com', password: hashedPassword });
+
+//       const courseData = {
+//         title: 'New Course',
+//         description: 'Description of the new course',
+//         contentText: 'Content text',
+//         contentUrl: 'https://example.com/content'
+//       };
+
+//       const res = await agent // Use the authenticated agent
+//         .post('/add-course')
+//         .send(courseData);
+//       expect(res.status).toBe(302);
+//       expect(res.header.location).toBe('/admin-dashboard');
+//     });
+// });
